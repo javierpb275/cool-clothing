1872f9ae46ec3052787e7f3f2df2750168b9c4da
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 19:30:18 +0200

    in order to dynamically generate a title, we pass a title into our menu-item component as a props and instead of props and then props.title we destructure the title off of our props by doing ({title}).

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 
-const MenuItem = () => (
-    <div className='menu-item'>
-        <div className='content'>
-            <h1 className='title'>HATS</h1>
+const MenuItem = ({ title }) => (
+        <div className='menu-item'>
+            <div className='content'>
+            <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
