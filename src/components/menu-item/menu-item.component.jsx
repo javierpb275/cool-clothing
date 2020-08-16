60b8b7fd780a1a96fbd09bc50fe1b6cdbebdcdb2
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 19:24:10 +0200

    created menu-item component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+//This menu-item component is each of the images that you see in the homepage, inside of the directory component:
+
+import React from 'react';
+
+const MenuItem = () => (
+    <div className='menu-item'>
+        <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
