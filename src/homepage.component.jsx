63ecbd9dfa1c82448a50cb4428895690d79c061c
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 16:48:46 +0200

    created  containers for the component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const HomePage = () => (
-    <div>
-
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>      
     </div>
-)
+);
+
+export default HomePage;
