979e0404cd785f745b846960a05e33bc27a841ee
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 21:10:10 +0200

    set 2 sections to a different size by usign one of their properties which is size: 'large'. We pass that size as props to the menu-item and inside clasName we pass that prop and then in the styles sheet we say "&.large { height: 380px } inside of the .menu-item class selector.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({ title, imageUrl }) => (
-        <div style={{backgroundImage: `url(${imageUrl})`}} className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+        <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
             <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
