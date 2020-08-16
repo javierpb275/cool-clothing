ff17a004327750a4bb9a72a8a4121851d658314c
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 20:01:55 +0200

    This will be a class component because we need to store the state value of  the MenuItems that we are gonna pass and crete our MenuItems with. So this componet is a Directory with a state value of those MenuItems.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//STYLES:
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+}
