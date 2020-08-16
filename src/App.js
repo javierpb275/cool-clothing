5c75d6ce408a42fe212b29657ac88465e2e3a4b5
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-16 16:54:27 +0200

    imported HomePage component and added to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+//PAGES:
+import HomePage from './homepage.component';
+
 function App() {
   return (
     <div>
-     Hello World
+     <HomePage/>
     </div>
   );
 }
