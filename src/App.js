20ba64186610f641fa9d7b3471c03e6815a18885
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-17 19:01:20 +0200

    Route is component that takes a couple of arguments.  component={} is the component that we wanna render. path='/' is a string that is equal to the path itself. Because we are in our app the path will be localhost:3000 because we just put '/' so what it does is when we are at the base "/" (localhost:3000) we wanna render the homepage.exact is a true or false property that says that it has to be exactly that path to render that component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+
+//React-Router-Dom:
+import { Route } from 'react-router-dom';
+
+//STYLES:
 import './App.css';
 
 //PAGES:
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
