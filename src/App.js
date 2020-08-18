db2c71bec34c948b9c58f492f022acbc1f6c4161
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-18 13:23:34 +0200

    we can use the "match" property to change to a different page depending on the id that we pass in the url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //React-Router-Dom:
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 //STYLES:
 import './App.css';
@@ -10,9 +10,18 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 
-const HatsPage = () => (
+const HatsPage = (props) => (
   <div>
     <h1>HATS PAGE</h1>
+    <Link to={`${props.match.url}/13`}>hat 13</Link>
+    <Link to={`${props.match.url}/14`}>hat 14</Link>
+    <Link to={`${props.match.url}/15`}>hat 15</Link>
+  </div>
+);
+
+const Hat = (props) => (
+  <div>
+    <h1>HAT NUMBER: {props.match.params.hatId}</h1>
   </div>
 );
 
@@ -21,7 +30,8 @@ function App() {
     <div>
       <Switch>
       <Route  exact path='/' component={HomePage} />
-      <Route  path='/hats' component={HatsPage} />
+      <Route  exact path='/hats' component={HatsPage} />
+      <Route  path='/hats/:hatId' component={Hat} />
       </Switch>
     </div>
   );
