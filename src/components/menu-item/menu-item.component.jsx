237d5305763da0554c8ab9bd929e09de8c9f4145
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-18 15:48:31 +0200

    We now have acces to out linkUrl which we want to push. However we don't know in our MenuItem where in our Directory we will be. So we will use also match.url. If we were to change our URL at any point, our component is still fully aware and functional because it knows that it's just going to base where it's been matched up to  and then append the URL (/somematchedURL/linkURL).

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,8 +8,8 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
-        <div className={`${size} menu-item`} onClick={() => history.push()} >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
             <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
             <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
