b790380aa21374ce6c5704343d0edbfec8c58a8c
javierpb275 <javierpb275@gmail.com>
2020-08-20 16:42:42 +0200

    created CollectionPreview component to display previews of our collections in the ShopPage component.

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,9 @@
+//This component is a preview of four items of each collection and it will be display in our ShopPage component.
+
+import React from 'react';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = () => (
+    <div></div>
+)
