1ea7629c40795f170a6c20c7472f0058a811e867
javierpb275 <javierpb275@gmail.com>
2020-08-20 17:25:14 +0200

    we filtered our list of items so that it only displays 4.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,7 +11,7 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.map(item => (
+                items.filter((item, idx) => idx < 4).map(item => (
 
                 <div key={item.id}> 
                 {item.name}
