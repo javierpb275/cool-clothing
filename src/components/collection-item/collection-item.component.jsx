88b5c3023dded56ad13e8c0ceb99e061b12925fa
javierpb275 <javierpb275@gmail.com>
2020-08-20 18:27:47 +0200

    placed all the props in its tag

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,9 +5,14 @@ import React from 'react';
 //STYLES:
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
+const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-item'>
-        <div className='image'></div>
+        <div className='image' style={{ backgroundImage: `url(${imageUrl})` }}/>
+            <div className='collection-footer'>
+                <span className='name'>{ name }</span>
+                <span className='price'>{ price }</span>
+            </div>
+        
     </div>
 )
 
