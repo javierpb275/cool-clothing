9ff696da990f29a6abea4883f76edb0756105850
javierpb275 <javierpb275@gmail.com>
2020-08-21 17:24:54 +0200

    created header component  which is the top of our website where we will put the Sign In, Shop, the logo and the cart that link to other pages.

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+//This component is the top of our website where we have all the links (Sign In, Shop...), the cart and the logo of our App
+
+import React from 'react';
+
+//STYLES: 
+import './header.styles.scss';
+
+const Header = () => (
+    <div></div>
+);
+
+export default Header;
