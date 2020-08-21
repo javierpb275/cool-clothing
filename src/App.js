e16656be09866bb4c88b340c11b48b2a027de8d0
javierpb275 <javierpb275@gmail.com>
2020-08-21 18:28:32 +0200

    imported header component in App.js and put it outside of the switch and the routes so that it is always present in all our pages

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import './App.css';
 
 //PAGES:
 import HomePage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop/shop.component';
 
-
+//COMPONENTS:
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route  exact path='/' component={HomePage} />
       <Route  path='/shop' component={ShopPage} />
