c62b5311986e4230d539bb9f6ac920e7705e98b6
javierpb275 <javierpb275@gmail.com>
2020-08-22 20:19:59 +0200

    created SignInAndSignUpPage component for our users to be able to sign in or register. It will be displayed both for SignIn or SignUp and the user will be able to SignIn with Google.

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,16 @@
+//This component is our sign in and sign up page where the user can enter with their account or register a new user.
+//The user can sign in with google instead of  typing all their info or having to register.
+
+import React from 'react';
+
+//STYLES:
+import './sign-in-and-sign-up.styles.scss';
+
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        SIGN IN
+    </div>
+);
+
+export default SignInAndSignUpPage;
