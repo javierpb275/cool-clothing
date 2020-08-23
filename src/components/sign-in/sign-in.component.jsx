fc9ef19843cb7562a9e2d853161313fba31f3eac
javierpb275 <javierpb275@gmail.com>
2020-08-23 17:55:13 +0200

    created handleChange() function where we pull both the value and the name off of our event.target. The event.target will end up being the <input/> element itself and we want the name on that input (name='password') as well as the value (value={this.state.email}). we dinamically setState so that sets that name by doing "[name]"     //so it will render whatever value that "name" it will be. If "name=password" it will point to password and the same with email.     //This way we can pass the same function to both inputs

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -23,16 +23,43 @@ class SignIn extends React.Component {
         this.setState({ email: '', password: '' })
     }
 
+    //This function pulls both value and name off of our event.target (what the user types). 
+    //The event.target will end up being the <input/> element itself and we want the name on that input (name='password') 
+    //as well as the value (value={this.state.email}). And we dinamically setState so that sets that name by doing "[name]"
+    //so it will render whatever value that "name" it will be. If "name=password" it will point to password and the same with email.
+    //This way we can pass the same function to both inputs
+    handleChange = event => {
+        const { value, name } = event.target;
+        
+        this.setState({[name]: value})
+    }
+
     render() {
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
+
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" value={this.state.email} required/>
+
+                    <input 
+                    name="email" 
+                    type="email" 
+                    value={this.state.email} 
+                    onChange={this.handleChange}
+                    required
+                    />
                     <label>Email</label>
-                    <input name="password" value={this.state.password} required/>
+
+                    <input 
+                    name="password" 
+                    type="password" 
+                    value={this.state.password} 
+                    onChange={this.handleChange}
+                    required
+                    />
                     <label>Password</label>
+
                     <input type="submit" value="Submit Form"/>
                 </form>
             </div>
