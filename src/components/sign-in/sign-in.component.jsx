ca6ca2db21f3b61950349305e9207343bd555eb4
javierpb275 <javierpb275@gmail.com>
2020-08-23 17:29:04 +0200

    created handleSubmit() function which for now will simply prevent the default submit action from firing because we want full control over exactly what this submit is gonna do. And then we will clear out our fields "this.setState({ email: '', password: '' })". and we pass the function to onSubmit. INFO ABOUT PREVENTDEFAULT: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -15,12 +15,20 @@ class SignIn extends React.Component {
         }
     }
 
+    //This function prevents the default submit action from firing because we want full control over exactly what this submit is gonna do. 
+    //And then we will clear out our fields "this.setState({ email: '', password: '' })"
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' })
+    }
+
     render() {
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input name="email" value={this.state.email} required/>
                     <label>Email</label>
                     <input name="password" value={this.state.password} required/>
