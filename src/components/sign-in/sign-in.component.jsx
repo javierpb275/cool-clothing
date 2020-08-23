71f6ddd19468789630f39151a716ce9e8392e89a
javierpb275 <javierpb275@gmail.com>
2020-08-23 17:18:29 +0200

    added a simple form to the SignIN component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -20,6 +20,13 @@ class SignIn extends React.Component {
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
+                <form>
+                    <input name="email" value={this.state.email} required/>
+                    <label>Email</label>
+                    <input name="password" value={this.state.password} required/>
+                    <label>Password</label>
+                    <input type="submit" value="Submit Form"/>
+                </form>
             </div>
         );
     }
