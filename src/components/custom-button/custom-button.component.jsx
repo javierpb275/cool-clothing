e61abd22d67ea750572ee64cd4f62c807cbde728
javierpb275 <javierpb275@gmail.com>
2020-08-24 16:32:24 +0200

    created custom-button component. It is areusable button component that we can use in our entire app.

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+//This button is a reusable button component that we use in different places in our app.
+
+import React from 'react';
+
+//STYLES:
+import './custom-button.styles.scss';
+
+const CustomButton = () => (
+    <button className='custom-button'></button>
+);
