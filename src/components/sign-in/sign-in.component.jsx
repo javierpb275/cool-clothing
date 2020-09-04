3c1c6c0575b42d1e2e29efba181acc7b54409ffa
javierpb275 <javierpb275@gmail.com>
2020-09-04 18:22:02 +0200

    imported signInWithGoogle from firebase and created new CustomButton with and onClick with that function so that we can sign in with our google account. Now when we click on the button a window will open to select our google account.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,6 +7,9 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+//FIREBASE:
+import { signInWithGoogle } from '../../firebase/firebase.utils';//Authentication
+
 //STYLES:
 import './sign-in.styles.scss';
 
@@ -63,6 +66,10 @@ class SignIn extends React.Component {
 
                     <CustomButton type="submit"> 
                     Sign In 
+                    </CustomButton>
+
+                    <CustomButton onClick={signInWithGoogle} > 
+                    Sign In With Google
                     </CustomButton>
                     
                 </form>
