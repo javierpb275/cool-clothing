e5ae7d60556f199ca00b9172bdeeff728291e30a
javierpb275 <javierpb275@gmail.com>
2020-09-04 17:31:59 +0200

    imported firestore for database and auth for the authentication

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,1 +1,4 @@
-import firebase from 'firebase/app';
+//FIREBASE:
+import firebase from 'firebase/app';// This is the base import of firebase which gives us access to firestore and auth.
+import 'firebase/firestore';// firestore is for the database
+import 'firebase/auth';// auth is for the authentication
