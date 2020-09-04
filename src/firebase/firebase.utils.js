7fedea0dc899be0e6e7fac91e06de992647c8680
javierpb275 <javierpb275@gmail.com>
2020-09-04 17:39:52 +0200

    added object that we get from firebase when we create our project in firebase so that we can connect this react project to the firebase project. This object is the 'firebase SDK snippet. Also used initializeApp(config) so that we initialize firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,3 +2,16 @@
 import firebase from 'firebase/app';// This is the base import of firebase which gives us access to firestore and auth.
 import 'firebase/firestore';// firestore is for the database
 import 'firebase/auth';// auth is for the authentication
+
+const config = {
+    apiKey: "AIzaSyD_NlIw3bo0gQ33rA2ELrGPNiJeLaAmg4s",
+    authDomain: "cool-clothing-db-7b987.firebaseapp.com",
+    databaseURL: "https://cool-clothing-db-7b987.firebaseio.com",
+    projectId: "cool-clothing-db-7b987",
+    storageBucket: "cool-clothing-db-7b987.appspot.com",
+    messagingSenderId: "234467415521",
+    appId: "1:234467415521:web:2064a99ec38ca41363aea0",
+    measurementId: "G-3YJLXHG89M"
+  };
+
+firebase.initializeApp(config);
