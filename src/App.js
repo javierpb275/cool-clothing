6f716d6c249a8e3fb1a52fe656fd999e97807211
javierpb275 <javierpb275@gmail.com>
2020-09-05 20:36:21 +0200

    We also have to close subscriptions when this unmounts because we don't want any memory leaks in our JS application. SO we set a property called unsubscribeFromAuth on our class. Then we say this.unsubscribeFromAuth = auth.onAuthStateChanged whihch gives us back a function that when we call closes the script subscription. AND we wanna close it whenever our component unmounts (componentWillUnmount). AND this.unsubscribeFromAuth(); will close the subscription. THIS IS HOW WE HANDLE OUR APP BEING AWARE OF ANY AUTH CHANGES ON FIREBASE.

--- src/App.js
+++ src/App.js
@@ -27,13 +27,18 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user});
       console.log(user);
     });
   }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
   render() {
     return (
