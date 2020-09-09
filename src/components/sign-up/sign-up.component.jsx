fa0ecaa11d72a73305c1d44e046545810962d9c1
javierpb275 <javierpb275@gmail.com>
2020-09-09 14:46:42 +0200

    we create our form which will be very similar to our SignIn form. We pass a function called handleSubmit, which we haven't created yet . And we add a FormInput, the first will be the displayName. And we destructure off the values from our state that we pass into our FormInput component. we pass handleChange function that we haven't written yet. And the label is "display name" and it will be required.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -25,10 +25,24 @@ class SignUp extends React.Component {
     }
 
     render() {
+        const { displayName } = this.state;
         return(
             <div className='sign-up'>
+
                 <h2 className='title'>I do not have an account</h2>
                 <span>Sign up with your email and password</span>
+
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+
+                <FormInput
+                type='text'
+                name='displayName'
+                value={displayName}
+                onChange={this.handleChange}
+                label='Display Name'
+                required/>
+
+                </form>
             </div>
         )
     }
