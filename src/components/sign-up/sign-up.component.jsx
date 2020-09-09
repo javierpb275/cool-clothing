c3788555033b6a991b40ba0f09caf45a170286bc
javierpb275 <javierpb275@gmail.com>
2020-09-09 14:19:14 +0200

    imported FormInput and CustomButton components and also imported auth and createUserProfileDocument from firebase.utils since we are creating and authenticating new users.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,5 +1,14 @@
 //This component is a signup form where the user can sign up with its user account and register:
+
 import React from 'react';
+
+//COMPONENTS:
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+//FIREBASE:
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 //STYLES:
 import './sign-up.styles.scss';
+
