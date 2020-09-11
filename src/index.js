86eeaa7ad74f6f509ad05cc40ac04684c2136fec
javierpb275 <javierpb275@gmail.com>
2020-09-11 18:56:56 +0200

    imported Provider. Provider is a component that we wrap around the entire app because we want everything inside to have access to the store object that we get from redux.  Provider is the parent if everything inside of our app and it allows us to get access to all of the things related to the store (redux state).

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 //React-Router-Dom:
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';//Provider: It gives us access to all of the things related to the store object (redux state)
 
 //STYLES:
 import './index.css';
@@ -13,9 +14,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
