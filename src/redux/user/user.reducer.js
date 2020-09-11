e29d93037a65635bbfa55fafe43c6e9ee4b8ad60
javierpb275 <javierpb275@gmail.com>
2020-09-11 19:54:19 +0200

    The case that we care about is SET_CURRENT_USER.
    Whenever SET_CURRENT_USER is the action type that gets fired, we wanna return a new object which represents the new state.
    
    We spread in everything on the state (...state) becasue we only wanna modify the values that we care about and that is currentUser.
    
    The action object is just the type and then a payload. So we are setting the current user value with this payload.
    
    And we export default userReducer.

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -8,10 +8,16 @@ const INITIAL_STATE = {
 //The userReducer is the reducer that will store the state of our currentUser: 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case '':
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
 
         default:
             return state;
 
     }
 }
+
+export default userReducer;
