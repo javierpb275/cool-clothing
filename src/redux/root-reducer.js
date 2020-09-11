e74b6cc4d5aacbf5402b8a2761948616607df23e
javierpb275 <javierpb275@gmail.com>
2020-09-11 20:02:57 +0200

    In order to combine all the reducers together, we need to import "combineReducers".
    Imported userReducer inside root-reducer.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,1 +1,7 @@
-//This is the base reducer that represents all of the state of our app. This is the code that combines all of our other states together.
+//This is the base reducer that represents all of the state of our app, the overall reducer base on all of the reducers that it pulls in. 
+//This is the code that combines all of our other states (reducers) together.
+
+import { combineReducers } from 'redux';//combineReducers, as the name suggest, combines all of our reducers together.
+
+//REDUCERS:
+import userReducer from './user/user.reducer';
