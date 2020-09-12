c701d92c02aa221ed950dd18d0cc7a9612299aeb
javierpb275 <javierpb275@gmail.com>
2020-09-12 15:17:20 +0200

    we have to set up our middlewares. the middlewares that the store is expecting from redux is going to be an array .
    In order to make our store we do:
    const store = createStore(rootReducer, applyMiddleware(...middlewares));
    
    createStore() gets a rootreducer and the return value of applyMiddleware() where we spread in all our middlewares that we use.
    
    And then we export default store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,21 @@
 import { createStore, applyMiddleware } from 'redux';
 
 //MIDDLEWARE:
-import logger from 'redux-logger';//redux-logger (middleware) catches the action, console logs it out for us and then moves it along
+import logger from 'redux-logger';//logger (middleware) catches the action, console logs it out for us and then moves it along
 
 //REDUCERS:
 import rootReducer from './root-reducer';
+
+
+
+//Array of middlewares:
+const middlewares = [logger];
+
+
+//Store:
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+
+export default store;
+
