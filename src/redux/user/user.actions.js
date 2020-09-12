f8804014b518aa88b3435018d66fbee0e14fc8ad
javierpb275 <javierpb275@gmail.com>
2020-09-12 15:56:53 +0200

    we are gonna set user as the payload. and with this we created our user action.
     So we have the actions that our components are gonna leverage in order to update our reducers with the appropiate value.
    
     Now that we've finished our setup, we're gonna bring this into our components so we can start leveraging not only the new user created but also thus currentUser value inside of our reducer.

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,7 @@
 //These are the actions (functions that return objects) related to our user.
 
+//This action sets our current user:
 export const setCurrentUser = user => ({
     type: 'SET_CURRENT_USER',
-
+    payload: user
 });
