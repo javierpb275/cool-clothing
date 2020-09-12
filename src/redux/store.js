f14599ac7385c28c1db5cdda2bbd1505172db990
javierpb275 <javierpb275@gmail.com>
2020-09-12 15:03:30 +0200

    Redux-logger (middleware) catches the action, console logs it out for us and then moves it along.
    Also imported root-reducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,3 +2,9 @@
 
 //REDUX:
 import { createStore, applyMiddleware } from 'redux';
+
+//MIDDLEWARE:
+import logger from 'redux-logger';//redux-logger (middleware) catches the action, console logs it out for us and then moves it along
+
+//REDUCERS:
+import rootReducer from './root-reducer';
