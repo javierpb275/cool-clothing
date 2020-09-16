28aba6c93584de7cfb67be3d50a9acc55e979536
javierpb275 <javierpb275@gmail.com>
2020-09-16 17:56:35 +0200

    added shopping-bag.svg to assets. created cart-icon.component and cart-icon.styles files and added to components folder cart-icon folder. imported react, ShoppingIcon (shopping-bag image) and styles in our cart-icon.component.

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,11 @@
+//This is the cart component, which is a shopping bag where we see the number of items we added to our cart. 
+//And when we click on it, we display a dropdown menu where we see the image of our item, the quantity and the price.
+//And we can access to another page where we pay the items.
+
+import React from 'react';
+
+//ASSETS:
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+//STYLES:
+import '.cart-icon.styles.scss';
