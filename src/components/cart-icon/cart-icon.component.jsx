f4257336bb4bbefce5200c731205de1d0c2d4d79
javierpb275 <javierpb275@gmail.com>
2020-09-16 18:00:16 +0200

    created CartIcon functional component.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,3 +9,12 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 //STYLES:
 import '.cart-icon.styles.scss';
+
+const CartIcon = () => (
+    <div className='cart-icon'>
+        <ShoppingIcon className='shopping-icon'/>
+        <span className='item-count'>0</span>
+    </div>
+);
+
+export default CartIcon;
