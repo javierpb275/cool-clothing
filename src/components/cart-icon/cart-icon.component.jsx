c2129f207344bd0b97816a0dea76a2774fdfff26
javierpb275 <javierpb275@gmail.com>
2020-09-17 18:22:53 +0200

    passed toggleCartHidden action to our CartIcon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,17 +4,33 @@
 
 import React from 'react';
 
+//REDUX:
+import { connect } from 'react-redux';
+
+//ACTIONS:
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
 //ASSETS:
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 //STYLES:
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-    <div className='cart-icon'>
+
+
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
 );
 
-export default CartIcon;
+
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+
+
+export default connect(null, mapDispatchToProps)(CartIcon);
