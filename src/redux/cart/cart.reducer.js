b18048f2d5b91c227e4d063fe11e8b25d1d2b693
javierpb275 <javierpb275@gmail.com>
2020-09-17 17:54:58 +0200

    created cartReducer with case TOGGLE_CART_HIDDEN.
    The reason we wanna use toggle vs set is because our cart component will either be in a true or false state, so it is either hidden or not hidden.
    
    hidden: !state.hidden =
    whatever this boolean value is, I want you to convert it to the opposite of it. So if it is true, i want it to be false, if it is false, i want it to be true.
    
    And by default we just return the current state.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,3 +2,19 @@
 const INITIAL_STATE = {
     hidden: true
 }
+
+//The cart reducer will take care of all the functionality of our cart-icon component:
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            }
+
+            default:
+                return state;
+    }
+}
+
+export default cartReducer;
