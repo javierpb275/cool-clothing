f8832388608c3c7b04cae08f7d2288158e044b38
javierpb275 <javierpb275@gmail.com>
2020-09-17 18:40:08 +0200

    ({user: { currentUser }, }): This is the syntax you write when you wanna destructure nested values. So we are saying: I want this value (currentUser) off of our 'user', which is being destructureed off of our state.
    And we do the same thing with cart: { hidden }

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -21,7 +21,7 @@ import Logo from '../../assets/logo-cool-clothing.png';
 //STYLES: 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
 
         <Link className='logo-container' to='/' >
@@ -52,8 +52,9 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
