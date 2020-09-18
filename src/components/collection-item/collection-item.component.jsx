95459ac98970c62beb7614e9f60279bd04917836
javierpb275 <javierpb275@gmail.com>
2020-09-18 18:24:04 +0200

    and we add the addItem() function to the CustomButton and pass the 'item' that we needed.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -20,12 +20,17 @@ const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
     return (
     <div className='collection-item'>
+
         <div className='image' style={{ backgroundImage: `url(${imageUrl})` }}/>
             <div className='collection-footer'>
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
-            <CustomButton inverted> ADD to cart</CustomButton>
+
+            <CustomButton onClick={() => addItem(item)} inverted> 
+            ADD to cart
+            </CustomButton>
+            
     </div>
     )
 };
