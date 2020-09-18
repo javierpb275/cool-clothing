75e94ab37807779ed4c78631c15cffaddafd643b
javierpb275 <javierpb275@gmail.com>
2020-09-18 16:12:10 +0200

    we are gonna invert the colors based on a property, just like we did when we added this custom class pf 'google-sign-in'.
    So we add an 'inverted' prop that does the exact same thing as our 'google-sign-in'.
    
    inverted ? 'inverted' : '' =
    We are saying: If inverted is true, then render the inverted styles, if its false, then render nothing (' ')

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,12 @@
-//This button is a reusable button component that we use in different places in our app.
+//This button is a reusable button component that we use in different places in our application:
 
 import React from 'react';
 
 //STYLES:
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={`${inverted ? 'inverted' : ''}  ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
