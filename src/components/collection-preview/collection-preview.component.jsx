c878622c3c1b9ca4a203d7193ffe934f5d83704e
javierpb275 <javierpb275@gmail.com>
2020-09-18 18:13:02 +0200

    So first we need to see where we're using our collection item, which is our collection preview .
    So we pass the whole 'item':
    items.filter((item, idx) => idx < 4)
                    .map((item) => (
    
                    <CollectionItem key={item.id} item={item}/>
    
                    ))
    
    Now in our collection item we have access to our item object...

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,20 +10,24 @@ import CollectionItem from '../collection-item/collection-item.component';
 //STYLES:
 import './collection-preview.styles.scss';
 
+
+
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 items.filter((item, idx) => idx < 4)
-                .map(({id, ...otherItemProps}) => (
+                .map((item) => (
 
-                <CollectionItem key={id} {...otherItemProps}/> 
+                <CollectionItem key={item.id} item={item}/> 
                 
                 ))
             }
         </div>
     </div>
-)
+);
+
+
 
 export default CollectionPreview;
