ce7586fc8a75794304d25d70479422e4b7ff01e5
javierpb275 <javierpb275@gmail.com>
2020-09-18 17:03:55 +0200

    We are gonna have to add both the new actions and the new action types so that our reducers are aware of what addItem action we're trying to make.

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 //These are the types for our cart actions so that they are constant and we pass them to our actions and reducer:
 
 export const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 }
