0bbe2621bb535eeb0e8376c965ea2c03173ec0a5
javierpb275 <javierpb275@gmail.com>
2020-09-19 17:32:05 +0200

    we check if that cart item already exists: if (existingCartItem)
    cartItems.map() will return us a new array and we need to return new versions of our state
    so that our components know to re-render properly.
    
    we check if the we already have that cartItem: cartItem.id === cartItemToAdd.id?
    
    true: {...cartItem, quantity: cartItem.quantity + 1}
    we increase the amount (we don't have a quantity yet but we'll solve that later).
    
    false: cartItem
    we just wanna return the original item because there's no need to update any of the componentsthat might be relying on our object.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,13 @@
 //Here we can add any utility functions related to our cart redux code
 
 export const addItemToCart = (cartItems, cartItemToAdd) => {
+
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id ?
+            {...cartItem, quantity: cartItem.quantity + 1} 
+            : cartItem);
+    }
+
 };
