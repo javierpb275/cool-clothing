6e0a595e47d67776e111c7cb2f40d0a5f5b2b3c9
javierpb275 <javierpb275@gmail.com>
2020-09-19 17:20:36 +0200

    We are gonna look inside of our existing cart items to see if that cart item already exists: const existingCartItem = cartItems.find();
    cartItems.find() will return you the first item found in our array based off a condition that we pass in find(here).
    
    we get each individual item (cartItem) and we check the cartItem id if it matches the cartItemToAdd id:
    cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
    
    If it matches, it will set that cartItem where this condition (cartItem.id === cartItemToAdd.id) is true to our constant 'existingCartItem'. If it doesn't find anything, it will be undefined.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
 //Here we can add any utility functions related to our cart redux code
 
-export const addItemToCart = (cartItems, cartItemToAdd) => {};
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+};
