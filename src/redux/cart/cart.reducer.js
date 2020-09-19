e8c04a107717c8895d0dfe1737df812903593e36
javierpb275 <javierpb275@gmail.com>
2020-09-19 17:51:39 +0200

    imported addItemToCart() utility function in cart reducer.
    we replace this code:
    [...state.cartItems, action.payload]
    
    for this:
    addItemToCart(state.cartItems, action.payload)
    
    state.cartItems: the already existing items in our array.
    
    action.payload: the item that we wanna add.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,8 @@
 //TYPES:
-import { CartActionTypes } from './cart.types'
+import { CartActionTypes } from './cart.types';
+
+//UTILITY FUNCTIONS:
+import { addItemToCart } from './cart.utils';
 
 //This is the initial state of this reducer:
 const INITIAL_STATE = {
@@ -19,7 +22,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
 
             default:
