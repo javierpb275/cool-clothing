f87f95c681ec33a2aad93bc1532434c749e028a8
javierpb275 <javierpb275@gmail.com>
2020-09-19 17:43:26 +0200

    If the cartItem is not found inside of our array then:
    we return a new array with all of the existing items that were already thereand also add an object  which is the item we wanna add (cartItemToAdd) with a based quantity of 1:
    return [...cartItems, {...cartItemToAdd, quantity: 1}];
    
    This way, whenever we add our cartItems in, any subsequent cartItems we'll be able to reference that quantity value that we have added to our objects when we store them.
    
    (quantity property gets attached the first time around since this if block will not run when it's a new item).

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,8 @@
 //Here we can add any utility functions related to our cart redux code
 
+
+//This utility function checks if we already have the item that we wanna add in our array, if we have it, then we increase the amount,
+//if we don't have it, then we add that item to our array:
 export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
@@ -9,5 +12,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             {...cartItem, quantity: cartItem.quantity + 1} 
             : cartItem);
     }
+
+    return [...cartItems, {...cartItemToAdd, quantity: 1}];
 
 };
