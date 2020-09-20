fae1739c265ebcaff6f478dc29e9a14a8076d19c
javierpb275 <javierpb275@gmail.com>
2020-09-20 13:54:00 +0200

    created cartItem component and added the quantity property.

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,6 +5,16 @@ import React from 'react';
 //STYLES:
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: {imageUrl, price, name} }) => (
-    <div></div>
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+    <div className='cart-item'>
+        <img src={imageUrl} alt='item'/>
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'>
+                {quantity} x ${price}
+            </span>
+        </div>
+    </div>
 );
+
+export default CartItem;
