6250f5f8015872a21353503e60da7dfe2a15599c
javierpb275 <javierpb275@gmail.com>
2020-09-23 18:17:37 +0200

    import createSelector from 'reselect' library. 2 types of selectors: - input selector: does NOT use createSelector. - output selector: DOES use input selectors and createSelector to build themselves.

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,2 +1,5 @@
 //These are the selectors of our cart:
 //Selector: code that gets a state as in the whole state object and then pulls off just a small portion or slice of that state 
+
+//RESELECT:
+import { createSelector } from 'reselect';
