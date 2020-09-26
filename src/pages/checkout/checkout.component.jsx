c87bdd169dbed663fb2f6477dc2a4c612e3e10dd
javierpb275 <javierpb275@gmail.com>
2020-09-26 18:02:40 +0200

    added checkout header to cheackout page with each header block for Product description quantity and remove.
    Also added styles for checkout component.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,8 +6,24 @@ import './checkout.styles.scss';
 
 
 const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+    <div className='checkout-page'>
+        <div classname='checkout-header'>
+            <div classname='header-block'>
+                <span>Product</span>
+            </div>
+            <div classname='header-block'>
+                <span>Description</span>
+            </div>
+            <div classname='header-block'>
+                <span>Quantity</span>
+            </div>
+            <div classname='header-block'>
+                <span>Price</span>
+            </div>
+            <div classname='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
     </div>
 );
 
