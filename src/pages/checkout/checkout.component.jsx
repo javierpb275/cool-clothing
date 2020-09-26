654a6897cb356f4818fe30072f646c99e0040c9b
javierpb275 <javierpb275@gmail.com>
2020-09-26 17:01:09 +0200

    created checkout page component and added in App.js with its own route.

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//STYLES:
+import './checkout.styles.scss';
+
+
+
+const CheckoutPage = () => (
+    <div>
+        CHECKOUT PAGE
+    </div>
+);
+
+
+
+export default CheckoutPage;
