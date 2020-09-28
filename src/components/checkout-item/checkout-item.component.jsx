2f46ed29b83dbb83587c519e30dd6a491a1b2c4d
javierpb275 <javierpb275@gmail.com>
2020-09-28 19:10:32 +0200

    the reason why we use a container for our image is because it is easier for us to control de size of this section that holds our image.
    We also add the properties of that item.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,8 +11,12 @@ import './checkout-item.styles.scss';
 const CheckoutItem = () => (
     <div className='checkout-item'>
         <div className='image-container'>
-
+            <img alt='item'/>
         </div>
+        <span className='name'></span>
+        <span className='quantity'></span>
+        <span className='price'></span>
+        
     </div>
 );
 
