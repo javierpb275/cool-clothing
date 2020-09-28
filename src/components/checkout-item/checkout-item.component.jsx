f0a35e483044173c67eb1ade4ae80f6ddccf92d8
javierpb275 <javierpb275@gmail.com>
2020-09-28 19:26:25 +0200

    we pass the whole item as a prop: cartItem
    And we destructure off the other values that we need:
    cartItem: { name, imageUrl, price, quantity}
    
    And we pass them all inside of our component.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -8,14 +8,14 @@ import './checkout-item.styles.scss';
 
 
 
-const CheckoutItem = () => (
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity} }) => (
     <div className='checkout-item'>
         <div className='image-container'>
-            <img alt='item'/>
+            <img src={imageUrl} alt='item'/>
         </div>
-        <span className='name'></span>
-        <span className='quantity'></span>
-        <span className='price'></span>
+        <span className='name'>{name}</span>
+        <span className='quantity'>{quantity}</span>
+        <span className='price'>{price}</span>
         <div className='remove-button'>&#10054;</div>
     </div>
 );
