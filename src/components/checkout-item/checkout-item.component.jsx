6f385b09ab024b832a0b102c0f941af3e4f965a9
javierpb275 <javierpb275@gmail.com>
2020-09-28 19:04:54 +0200

    created checkout-item component and checkout-item styles.

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,21 @@
+//This component is the cart-item that we have stored in our state (the items that we added to our cart) 
+//which will be displayed in our checkout-page.
+
+import React from 'react';
+
+//STYLES:
+import './checkout-item.styles.scss';
+
+
+
+const CheckoutItem = () => (
+    <div className='checkout-item'>
+        <div className='image-container'>
+
+        </div>
+    </div>
+);
+
+
+
+export default CheckoutItem;
