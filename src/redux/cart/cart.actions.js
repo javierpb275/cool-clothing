d00ccbfabea8b9361652f80c8c3ecf39d433b584
javierpb275 <javierpb275@gmail.com>
2020-09-29 18:31:29 +0200

    created CLEAR_ITEM_FROM_CART action type.
    And created clearItemFromCart action.

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -13,3 +13,9 @@ export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
 });
+
+//This action takes care of removing the items that we added from our cart:
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
