497289fa39bdab2f068cc76316c4db408fff0274
javierpb275 <javierpb275@gmail.com>
2020-10-08 18:32:47 +0200

    created collection component.
    imported CollectionPage in the shop component.
    
    Added new Route to CollectionPage where the path will be: match.path(/shop) and then we add '/:collectionId' where we will pass an id of each collection so that we go to that spefic page (hats page) depending on the id of each collection.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,13 +8,14 @@ import { Route } from 'react-router-dom';
 
 //COMPONENTS:
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 
 const ShopPage = ({ match }) => (
 
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 
 );
