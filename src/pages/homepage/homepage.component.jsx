43956bd9b5be077c76599c435c7d883b5551cee2
javierpb275 <javierpb275@gmail.com>
2020-11-28 13:04:20 +0100

    created homepage.styles.jsx where we create our HomePageContainer for our homepafe styles. Imported that HomePageContainer in our homepage component and wrap our directory with that homepagecontainer component. This has the same result as before with the normal scss file. It is another way to add styles.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,12 +6,12 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
 //STYLES:
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-    <div className='homepage'>
+    <HomePageContainer>
         <Directory/>    
-    </div>
+    </HomePageContainer>
 );
 
 export default HomePage;
