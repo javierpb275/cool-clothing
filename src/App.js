4349608239b7cc53818513a5a40bebb841931221
javierpb275 <javierpb275@gmail.com>
2020-12-03 19:27:55 +0100

    collectionsArray.map(({title, items}) => ({ title, items }))
    we pass a new array where we get the object and we destrcuture off of it the title and the items and we return a new objetc where title goes to title and items goes to items.
    
    So we are passing only the values that we wanna keep.
    
    And this way we have saved our documents (items) inside of our collections inside of our firebase database.

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends React.Component {
       else {
 
         setCurrentUser(userAuth);
-        addCollectionAndDocuments('collections', collectionsArray);
+        addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({ title, items })));
 
       }
 
