7f8a2a54162bb5b71450bd4483c6e14e1c123e21
javierpb275 <javierpb275@gmail.com>
2020-12-05 12:11:07 +0100

    turned shop functional component into a class component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -11,14 +11,17 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 
 
-const ShopPage = ({ match }) => (
-
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-    </div>
-
-);
+class ShopPage extends React.Component {
+    render() {
+        const { match } = this.props;
+        return (
+            <div className='shop-page'>
+                <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+            </div>
+        );
+    }
+} 
     
 
 export default ShopPage;
