1d8d23d35093bf43829344f47edbb332aff4f463
javierpb275 <javierpb275@gmail.com>
2020-12-05 14:06:57 +0100

    we wanna store the collections object in our shop reducer because now…
    … that we have our modification we just wanna pass this and update our reducer.
    
    we are gonna update our reducer and redux code to now be able to fire actions that update the reducer.
    
    Now we have our new collections with the ids from our backend with the shape and format that we are looking for
    
    We've transformed our array into the map and updated our shop redux files to be able to update and store this new collections map object.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,6 +6,9 @@ import React from 'react';
 //REACT-ROUTER-DOM:
 import { Route } from 'react-router-dom';
 
+//REACT-REDUX:
+import { connect } from 'react-redux';
+
 //COMPONENTS:
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
@@ -13,16 +16,19 @@ import CollectionPage from '../collection/collection.component';
 //FIRESTORE
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+//SHOP ACTIONS:
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
         collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            console.log(collectionsMap);
+            updateCollections(collectionsMap);
         });
 
     }
@@ -38,5 +44,9 @@ class ShopPage extends React.Component {
     }
 } 
     
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
 
-export default ShopPage;
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,21 +1,23 @@
 //List of products of our shop:
 import SHOP_DATA from './shop.data';
 
-
+//SHOP ACTION TYPES:
+import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
     collections: SHOP_DATA
 };
 
-
-
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+            }
         default:
             return state;
     }
 };
-
-
 
 export default shopReducer;
