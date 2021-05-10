4a71dfe0f68c9a0b6221c6244fe295e730a35063
javierpb275 <javierpb275@gmail.com>
2021-05-10 18:54:40 +0200

    REDUX-THUNK
    installed redux-thunk.
    
    library that allows us to handle asynchronous event handling and firing multiple actions.
    
    imported thunk in store.js and added to the middlewares.
    
    thunk is a piece of middleware that allows us to fire functions.

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,7 @@ import { persistStore } from 'redux-persist';
 
 //MIDDLEWARE:
 import logger from 'redux-logger';//logger (middleware) catches the action, console logs it out for us and then moves it along
+import thunk from 'redux-thunk';//thunk is a piece of middleware that allows us to fire functions
 
 //REDUCERS:
 import rootReducer from './root-reducer';
@@ -15,7 +16,7 @@ import rootReducer from './root-reducer';
 
 
 //Array of middlewares:
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
