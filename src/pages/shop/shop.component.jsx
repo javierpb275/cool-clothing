b04cc67d47d68b8ae2b2bfaf5562f17e3b6026fa
javierpb275 <javierpb275@gmail.com>
2021-05-10 18:16:30 +0200

    FETCH PATTERN
    using the fetch pattern to get the colllections and log them in the console.
    
    to make a REST call we use the base API URL:
    
    https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/some_collection
    
    we can use fetch to get the objects that we need

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -34,11 +34,16 @@ class ShopPage extends React.Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
+        fetch('https://firestore.googleapis.com/v1/projects/cool-clothing-db-7b987/databases/(default)/documents/collections')
+        .then(response => response.json())
+        .then(collections => console.log(collections));
+/*
         collectionRef.get().then(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
         });
         this.setState({ loading: false});
+*/
     }
 
     render() {
