dd4786cf105e94c8299664cfac852c36142a582c
javierpb275 <javierpb275@gmail.com>
2021-05-16 11:52:35 +0200

    fixed issue in userReducer with the action types. wrong name of types used in the userReducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -10,15 +10,13 @@ const INITIAL_STATE = {
 //The userReducer is the reducer that will store the state of our currentUser: 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+        case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
                 currentUser: action.payload,
                 error: null
             }
-        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-        case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_IN_FAILURE:
             return {
                 ...state,
                 error: action.payload
